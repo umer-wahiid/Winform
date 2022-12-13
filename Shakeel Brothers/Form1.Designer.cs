b8abfb8d9e4a651829a98f5394436016ea911ff8
Umer Abdul Wahid <umerwahiid48@gmail.com>
2022-12-13 23:35:43 +0500

    Uploaded

---
+++ Shakeel Brothers/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Shakeel_Brothers
+{
+    partial class Startup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Manage = new System.Windows.Forms.Button();
+            this.users = new System.Windows.Forms.Button();
+            this.Transport = new System.Windows.Forms.Button();
+            this.Item = new System.Windows.Forms.Button();
+            this.Customer = new System.Windows.Forms.Button();
+            this.Area = new System.Windows.Forms.Button();
+            this.City = new System.Windows.Forms.Button();
+            this.CusPrint = new System.Windows.Forms.Button();
+            this.CreateBill = new System.Windows.Forms.Button();
+            this.Payments = new System.Windows.Forms.Button();
+            this.DelEntryView = new System.Windows.Forms.Button();
+            this.DelEntry = new System.Windows.Forms.Button();
+            this.OldRate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.EditPurBill = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.DoO = new System.Windows.Forms.Label();
+            this.BillByDate = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BillByNo = new System.Windows.Forms.Button();
+            this.BillByCust = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Manage
+            // 
+            this.Manage.BackColor = System.Drawing.Color.White;
+            this.Manage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Manage.Location = new System.Drawing.Point(39, 43);
+            this.Manage.Name = "Manage";
+            this.Manage.Size = new System.Drawing.Size(107, 37);
+            this.Manage.TabIndex = 0;
+            this.Manage.Text = "Manage";
+            this.Manage.UseVisualStyleBackColor = false;
+            // 
+            // users
+            // 
+            this.users.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.users.Location = new System.Drawing.Point(39, 101);
+            this.users.Name = "users";
+            this.users.Size = new System.Drawing.Size(99, 32);
+            this.users.TabIndex = 1;
+            this.users.Text = "users";
+            this.users.UseVisualStyleBackColor = true;
+            // 
+            // Transport
+            // 
+            this.Transport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Transport.Location = new System.Drawing.Point(39, 139);
+            this.Transport.Name = "Transport";
+            this.Transport.Size = new System.Drawing.Size(99, 33);
+            this.Transport.TabIndex = 2;
+            this.Transport.Text = "Transport";
+            this.Transport.UseVisualStyleBackColor = true;
+            // 
+            // Item
+            // 
+            this.Item.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Item.Location = new System.Drawing.Point(39, 178);
+            this.Item.Name = "Item";
+            this.Item.Size = new System.Drawing.Size(99, 31);
+            this.Item.TabIndex = 3;
+            this.Item.Text = "Item";
+            this.Item.UseVisualStyleBackColor = true;
+            // 
+            // Customer
+            // 
+            this.Customer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Customer.Location = new System.Drawing.Point(39, 215);
+            this.Customer.Name = "Customer";
+            this.Customer.Size = new System.Drawing.Size(99, 33);
+            this.Customer.TabIndex = 4;
+            this.Customer.Text = "Customer";
+            this.Customer.UseVisualStyleBackColor = true;
+            // 
+            // Area
+            // 
+            this.Area.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Area.Location = new System.Drawing.Point(39, 254);
+            this.Area.Name = "Area";
+            this.Area.Size = new System.Drawing.Size(99, 30);
+            this.Area.TabIndex = 5;
+            this.Area.Text = "Area";
+            this.Area.UseVisualStyleBackColor = true;
+            // 
+            // City
+            // 
+            this.City.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.City.Location = new System.Drawing.Point(40, 290);
+            this.City.Name = "City";
+            this.City.Size = new System.Drawing.Size(99, 29);
+            this.City.TabIndex = 6;
+            this.City.Text = "City";
+            this.City.UseVisualStyleBackColor = true;
+            // 
+            // CusPrint
+            // 
+            this.CusPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CusPrint.Location = new System.Drawing.Point(39, 325);
+            this.CusPrint.Name = "CusPrint";
+            this.CusPrint.Size = new System.Drawing.Size(99, 82);
+            this.CusPrint.TabIndex = 7;
+            this.CusPrint.Text = "Customer Print";
+            this.CusPrint.UseVisualStyleBackColor = true;
+            // 
+            // CreateBill
+            // 
+            this.CreateBill.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.CreateBill.Location = new System.Drawing.Point(163, 78);
+            this.CreateBill.Name = "CreateBill";
+            this.CreateBill.Size = new System.Drawing.Size(143, 147);
+            this.CreateBill.TabIndex = 8;
+            this.CreateBill.Text = "Create Bill";
+            this.CreateBill.UseVisualStyleBackColor = true;
+            // 
+            // Payments
+            // 
+            this.Payments.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Payments.Location = new System.Drawing.Point(163, 245);
+            this.Payments.Name = "Payments";
+            this.Payments.Size = new System.Drawing.Size(143, 74);
+            this.Payments.TabIndex = 9;
+            this.Payments.Text = "Payments";
+            this.Payments.UseVisualStyleBackColor = true;
+            // 
+            // DelEntryView
+            // 
+            this.DelEntryView.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.DelEntryView.Location = new System.Drawing.Point(167, 335);
+            this.DelEntryView.Name = "DelEntryView";
+            this.DelEntryView.Size = new System.Drawing.Size(134, 29);
+            this.DelEntryView.TabIndex = 10;
+            this.DelEntryView.Text = "Delete Entry View";
+            this.DelEntryView.UseVisualStyleBackColor = true;
+            // 
+            // DelEntry
+            // 
+            this.DelEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DelEntry.Location = new System.Drawing.Point(184, 377);
+            this.DelEntry.Name = "DelEntry";
+            this.DelEntry.Size = new System.Drawing.Size(99, 30);
+            this.DelEntry.TabIndex = 11;
+            this.DelEntry.Text = "Delete Entry";
+            this.DelEntry.UseVisualStyleBackColor = true;
+            // 
+            // OldRate
+            // 
+            this.OldRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OldRate.Location = new System.Drawing.Point(184, 413);
+            this.OldRate.Name = "OldRate";
+            this.OldRate.Size = new System.Drawing.Size(99, 30);
+            this.OldRate.TabIndex = 12;
+            this.OldRate.Text = "Old Rates";
+            this.OldRate.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BillByCust);
+            this.groupBox1.Controls.Add(this.BillByNo);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.BillByDate);
+            this.groupBox1.Controls.Add(this.DoO);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.EditPurBill);
+            this.groupBox1.Location = new System.Drawing.Point(329, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 307);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // EditPurBill
+            // 
+            this.EditPurBill.BackColor = System.Drawing.Color.White;
+            this.EditPurBill.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditPurBill.Location = new System.Drawing.Point(19, 21);
+            this.EditPurBill.Name = "EditPurBill";
+            this.EditPurBill.Size = new System.Drawing.Size(161, 30);
+            this.EditPurBill.TabIndex = 1;
+            this.EditPurBill.Text = "Edit Purchase Bill";
+            this.EditPurBill.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // DoO
+            // 
+            this.DoO.AutoSize = true;
+            this.DoO.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(220)))), ((int)(((byte)(180)))));
+            this.DoO.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.DoO.Location = new System.Drawing.Point(16, 59);
+            this.DoO.Name = "DoO";
+            this.DoO.Size = new System.Drawing.Size(44, 20);
+            this.DoO.TabIndex = 3;
+            this.DoO.Text = "DoO";
+            // 
+            // BillByDate
+            // 
+            this.BillByDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BillByDate.Location = new System.Drawing.Point(37, 89);
+            this.BillByDate.Name = "BillByDate";
+            this.BillByDate.Size = new System.Drawing.Size(119, 32);
+            this.BillByDate.TabIndex = 4;
+            this.BillByDate.Text = "Bill By Date";
+            this.BillByDate.UseVisualStyleBackColor = true;
+            this.BillByDate.Click += new System.EventHandler(this.BillByDate_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(66, 133);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(220)))), ((int)(((byte)(180)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(7, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Or.No";
+            // 
+            // BillByNo
+            // 
+            this.BillByNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BillByNo.Location = new System.Drawing.Point(37, 166);
+            this.BillByNo.Name = "BillByNo";
+            this.BillByNo.Size = new System.Drawing.Size(119, 32);
+            this.BillByNo.TabIndex = 7;
+            this.BillByNo.Text = "Bill By OR.No";
+            this.BillByNo.UseVisualStyleBackColor = true;
+            // 
+            // BillByCust
+            // 
+            this.BillByCust.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BillByCust.Location = new System.Drawing.Point(37, 216);
+            this.BillByCust.Name = "BillByCust";
+            this.BillByCust.Size = new System.Drawing.Size(119, 72);
+            this.BillByCust.TabIndex = 8;
+            this.BillByCust.Text = "Bill By Customer";
+            this.BillByCust.UseVisualStyleBackColor = true;
+            this.BillByCust.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Startup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1742, 760);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.OldRate);
+            this.Controls.Add(this.DelEntry);
+            this.Controls.Add(this.DelEntryView);
+            this.Controls.Add(this.Payments);
+            this.Controls.Add(this.CreateBill);
+            this.Controls.Add(this.CusPrint);
+            this.Controls.Add(this.City);
+            this.Controls.Add(this.Area);
+            this.Controls.Add(this.Customer);
+            this.Controls.Add(this.Item);
+            this.Controls.Add(this.Transport);
+            this.Controls.Add(this.users);
+            this.Controls.Add(this.Manage);
+            this.Name = "Startup";
+            this.Text = "Startup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Manage;
+        private System.Windows.Forms.Button users;
+        private System.Windows.Forms.Button Transport;
+        private System.Windows.Forms.Button Item;
+        private System.Windows.Forms.Button Customer;
+        private System.Windows.Forms.Button Area;
+        private System.Windows.Forms.Button City;
+        private System.Windows.Forms.Button CusPrint;
+        private System.Windows.Forms.Button CreateBill;
+        private System.Windows.Forms.Button Payments;
+        private System.Windows.Forms.Button DelEntryView;
+        private System.Windows.Forms.Button DelEntry;
+        private System.Windows.Forms.Button OldRate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BillByDate;
+        private System.Windows.Forms.Label DoO;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button EditPurBill;
+        private System.Windows.Forms.Button BillByCust;
+        private System.Windows.Forms.Button BillByNo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
