a07e950d815522946ed704affc18de74d1fe321c
Umer Abdul Wahid <umerwahiid48@gmail.com>
2022-12-14 22:37:19 +0500

    commit

---
+++ Shakeel Brothers/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Shakeel_Brothers
+{
+    internal class Class1
+    {
+        public SqlConnection con;
+
+        public Class1()
+        {
+            con = new SqlConnection("Data Source=DESKTOP-76GEPIK; user id=sa;password=a123456;Initial Catalog=shakeel_brothers");
+        }
+
+    }
+}
--- Shakeel Brothers/Login.cs
+++ Shakeel Brothers/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Shakeel_Brothers
 {
@@ -20,6 +21,27 @@ namespace Shakeel_Brothers
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 c = new Class1();
+            SqlCommand cmd = new SqlCommand("Select * from tblCashier where Cashier=@c and Password=@p",c.con);
+            cmd.Parameters.AddWithValue("@c",textBox1.Text);
+            cmd.Parameters.AddWithValue("@p", textBox2.Text);
+            c.con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                Startup s = new Startup();
+                s.Show();
+                this.Hide();
+            }
         }
     }
 }
