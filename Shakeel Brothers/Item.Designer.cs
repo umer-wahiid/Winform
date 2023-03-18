aac6f1341e37fc9a1444e05d75f05a0801323fe0
Umer Abdul Wahid <umerwahiid48@gmail.com>
2023-03-18 19:13:18 +0500

    item done

--- Shakeel Brothers/Item.Designer.cs
+++ Shakeel Brothers/Item.Designer.cs
@@ -29,26 +29,277 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtUqty = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUpacking = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUitem = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
             this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPacking = new System.Windows.Forms.TextBox();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(505, 427);
+            this.panel1.Size = new System.Drawing.Size(505, 506);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.txtId);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.txtUqty);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txtUpacking);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.txtUitem);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.btnUpdate);
+            this.panel2.Controls.Add(this.txtSearch);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.btnDel);
+            this.panel2.Controls.Add(this.txtRate);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.txtQty);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtPacking);
+            this.panel2.Controls.Add(this.txtItem);
+            this.panel2.Controls.Add(this.name);
+            this.panel2.Controls.Add(this.btnBack);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 325);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(505, 181);
+            this.panel2.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(293, 72);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 16);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "پیمانہ";
+            // 
+            // txtUqty
+            // 
+            this.txtUqty.Location = new System.Drawing.Point(187, 70);
+            this.txtUqty.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUqty.Name = "txtUqty";
+            this.txtUqty.Size = new System.Drawing.Size(102, 20);
+            this.txtUqty.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 73);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Pack Qty.";
+            // 
+            // txtUpacking
+            // 
+            this.txtUpacking.Location = new System.Drawing.Point(187, 46);
+            this.txtUpacking.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUpacking.Name = "txtUpacking";
+            this.txtUpacking.Size = new System.Drawing.Size(102, 20);
+            this.txtUpacking.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(293, 26);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 16);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "آیٹم";
+            // 
+            // txtUitem
+            // 
+            this.txtUitem.Location = new System.Drawing.Point(187, 22);
+            this.txtUitem.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUitem.Name = "txtUitem";
+            this.txtUitem.Size = new System.Drawing.Size(102, 20);
+            this.txtUitem.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 97);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Rate";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnUpdate.Location = new System.Drawing.Point(329, 138);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(56, 28);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(392, 42);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(102, 20);
+            this.txtSearch.TabIndex = 3;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(415, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Search";
+            // 
+            // btnDel
+            // 
+            this.btnDel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnDel.Location = new System.Drawing.Point(407, 138);
+            this.btnDel.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(56, 28);
+            this.btnDel.TabIndex = 6;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(62, 94);
+            this.txtRate.Margin = new System.Windows.Forms.Padding(2);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.ReadOnly = true;
+            this.txtRate.Size = new System.Drawing.Size(102, 20);
+            this.txtRate.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(293, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "پیکنگ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(92, 138);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(62, 70);
+            this.txtQty.Margin = new System.Windows.Forms.Padding(2);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(102, 20);
+            this.txtQty.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 49);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Packing";
+            // 
+            // txtPacking
+            // 
+            this.txtPacking.Location = new System.Drawing.Point(62, 46);
+            this.txtPacking.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPacking.Name = "txtPacking";
+            this.txtPacking.Size = new System.Drawing.Size(102, 20);
+            this.txtPacking.TabIndex = 1;
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(62, 22);
+            this.txtItem.Margin = new System.Windows.Forms.Padding(2);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(102, 20);
+            this.txtItem.TabIndex = 0;
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(30, 25);
+            this.name.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(27, 13);
+            this.name.TabIndex = 5;
+            this.name.Text = "Item";
+            // 
+            // btnBack
+            // 
+            this.btnBack.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnBack.Location = new System.Drawing.Point(18, 138);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(56, 28);
+            this.btnBack.TabIndex = 7;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // dataGridView1
             // 
@@ -60,46 +311,32 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(505, 360);
+            this.dataGridView1.Size = new System.Drawing.Size(505, 506);
             this.dataGridView1.TabIndex = 0;
             // 
-            // panel2
+            // txtId
             // 
-            this.panel2.Controls.Add(this.txtSearch);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 360);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(505, 67);
-            this.panel2.TabIndex = 0;
+            this.txtId.Location = new System.Drawing.Point(187, 95);
+            this.txtId.Margin = new System.Windows.Forms.Padding(2);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(102, 20);
+            this.txtId.TabIndex = 26;
             // 
-            // button1
+            // label8
             // 
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button1.Location = new System.Drawing.Point(11, 18);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 27);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Exit";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // txtSearch
-            // 
-            this.txtSearch.Location = new System.Drawing.Point(90, 22);
-            this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(130, 20);
-            this.txtSearch.TabIndex = 2;
-            this.txtSearch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(293, 98);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 13);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "ID";
             // 
             // Item
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.button1;
-            this.ClientSize = new System.Drawing.Size(505, 427);
+            this.ClientSize = new System.Drawing.Size(505, 506);
             this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Item";
@@ -108,9 +345,9 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Item_FormClosing);
             this.Load += new System.EventHandler(this.Item_Load);
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -118,9 +355,29 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtUqty;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUpacking;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUitem;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPacking;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtId;
     }
 }
--- Shakeel Brothers/Item.cs
+++ Shakeel Brothers/Item.cs
@@ -13,56 +13,148 @@ namespace Shakeel_Brothers
 {
     public partial class Item : Form
     {
-        SqlDataAdapter adap;
-        DataTable dt;
+        //select ID, RawName as 'Items', Packing, URawName as 'آئٹم کا نام', UPacking as 'پیکنگ', PQty as 'پیک مقدار', Measure as 'پیمانہ', PRate as 'پیک ریٹ'  from tblRawMaterial
+
 
         Class1 c = new Class1();
+
+        public void showgrid()
+        {
+            dataGridView1.DataSource = c.GetData("select ID, RawName as 'Items', Packing, URawName as 'آئٹم کا نام', UPacking as 'پیکنگ', PQty as 'پیک مقدار', Measure as 'پیمانہ', PRate as 'پیک ریٹ'  from tblRawMaterial");
+        }
+        public void clr()
+        {
+            txtItem.Text = "";
+            txtPacking.Text = "";
+            txtQty.Text = "";
+            txtRate.Text = "";
+            txtUitem.Text = "";
+            txtUpacking.Text = "";
+            txtUqty.Text = "";
+        }
+
         public Item()
         {
             InitializeComponent();
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void btnBack_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void Item_Load(object sender, EventArgs e)
-        {
-            adap = new SqlDataAdapter("select ID ,RawName as 'Items',Packing,URawName as 'آئٹم کا نام',UPacking as 'پیکنگ',PQty as 'پیک مقدار',Measure as 'پیمانہ', PRate as 'پیک ریٹ'  from tblRawMaterial", c.con);
-            dt = new DataTable();
-            adap.Fill(dt);
-            dataGridView1.DataSource = dt;
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            SqlCommandBuilder cmbdl = new SqlCommandBuilder(adap);
-            adap.Update(dt);
             this.Close();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
+            if (txtItem.Text != "" && txtPacking.Text != "" && txtUitem.Text != "" && txtUpacking.Text != "")
+            {
+                SqlCommand cmd = new SqlCommand("insert into tblRawMaterial(RawName,URawName,Packing,UPacking,PQty,Measure,PRate)values(@n,@un,@p,@up,@c,@m,@r)", c.con);
+                cmd.Parameters.AddWithValue("@n", txtItem.Text);
+                cmd.Parameters.AddWithValue("@un", txtUitem.Text);
+                cmd.Parameters.AddWithValue("@p", txtPacking.Text);
+                cmd.Parameters.AddWithValue("@up", txtUpacking.Text);
+                cmd.Parameters.AddWithValue("@c", txtQty.Text);
+                cmd.Parameters.AddWithValue("@m", txtUqty.Text);
+                cmd.Parameters.AddWithValue("@r", txtRate.Text);
+                c.IUD(cmd);
+                clr();
+                showgrid();
+                txtItem.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Please Insert Data to Save !!");
+            }
         }
 
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtItem.Text != "" && txtPacking.Text != "" && txtUitem.Text != "" && txtUpacking.Text != "" && txtId.Text != "")
+            {
+                SqlCommand cmd = new SqlCommand("update tblRawMaterial set RawName=@n,URawName=@un,Packing=@p,UPacking=@up,PQty=@c,Measure=@m,PRate=@r", c.con);
+                cmd.Parameters.AddWithValue("@i", txtId.Text);
+                cmd.Parameters.AddWithValue("@n", txtItem.Text);
+                cmd.Parameters.AddWithValue("@un", txtUitem.Text);
+                cmd.Parameters.AddWithValue("@p", txtPacking.Text);
+                cmd.Parameters.AddWithValue("@up", txtUpacking.Text);
+                cmd.Parameters.AddWithValue("@c", txtQty.Text);
+                cmd.Parameters.AddWithValue("@m", txtUqty.Text);
+                cmd.Parameters.AddWithValue("@r", txtRate.Text);
+                c.IUD(cmd);
+                clr();
+                showgrid();
+                txtItem.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Please Insert Data to Update !!");
+            }
+        }
+
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (txtId.Text != "")
+            {
+                SqlCommand cmd = new SqlCommand("Delete from tblRawMaterial where ID=@i", c.con);
+                cmd.Parameters.AddWithValue("@i", txtId.Text);
+                c.IUD(cmd);
+                clr();
+                showgrid();
+            }
+            else
+            {
+                MessageBox.Show("Select Data Before Delete !!");
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = c.GetData("select * from tblRawMaterial Where RawName like '" + txtSearch.Text + "'+'%'");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+        private void Item_Load(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
         private void Item_FormClosing(object sender, FormClosingEventArgs e)
         {
-            SqlCommandBuilder cmbdl = new SqlCommandBuilder(adap);
-            adap.Update(dt);
         }
-
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            adap = new SqlDataAdapter("select ID ,RawName as 'Items',Packing,URawName as 'آئٹم کا نام',UPacking as 'پیکنگ',PQty as 'پیک مقدار',Measure as 'پیمانہ', PRate as 'پیک ریٹ'  from tblRawMaterial Where RawName like '" + txtSearch.Text + "'+'%'", c.con);
-            dt = new DataTable();
-            adap.Fill(dt);
-            dataGridView1.DataSource = dt;
         }
-
         private void textBox1_TextChanged_1(object sender, EventArgs e)
         {
-
         }
+        private void label6_Click(object sender, EventArgs e)
+        {
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
     }
 }
